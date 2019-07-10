adcc648bc578258ba338b82afdcc211e5593b718
Sarah Drake <jussaradrake@outlook.com>
2019-07-10 14:19:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -10,7 +10,8 @@
     new_array = []
     counter = 0
     while counter < array.length do
-    puts array[counter]
+    new_array << (array[counter] ** 2)
     counter +=1
     end
+    new_array
   end
