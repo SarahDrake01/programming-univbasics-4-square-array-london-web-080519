f38ec309eafd1609859fd6a3d23162682cbe45b5
Sarah Drake <jussaradrake@outlook.com>
2019-07-10 13:45:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
-  # your code here
-end
+#In this lab we will be building a method, square_array,
+#that squares each element in an array of numbers and returns a new array of these squared numbers.
+
+# 1. Write a method that square numbers in an array
+# 2 Need array & new array
+# 3. Use a While loop to ieterate over the array
+
+new_array = [1,2,3]
+ def square_array(array)
+   []
+   counter = 0
+   while counter < array.length do
+   array[counter]
+   counter +=1
+   end
