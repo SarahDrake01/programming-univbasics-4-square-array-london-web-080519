5bdee01355d9c7b03688ab0600c92aa32820a1b9
Sarah Drake <jussaradrake@outlook.com>
2019-07-10 13:55:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,9 +7,10 @@
 
 new_array = [1,2,3]
   def square_array(array)
-    []
+    new_array[]
     counter = 0
     while counter < array.length do
     puts array[counter]
     counter +=1
     end
+  end
